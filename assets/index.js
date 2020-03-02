dfe25efc37b80c70558d7f872815df6d7eed2954
Jesus Esquer <jme2791@gmail.com>
2020-03-02 12:08:01 -0700

    Updated javascript file added pseudo coding for each step in app

---
+++ assets/index.js
@@ -0,0 +1,14 @@
+// Select elements on page
+// Canvas
+
+// Shake button
+
+// Setup canvas for drawing
+
+// Write a draw function
+
+// Write a handler for keys
+
+// Clear/Shake function
+
+// Listen for arrow keys
