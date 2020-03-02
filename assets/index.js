24cc39069bfbca4243a4b52d211bc86997ab2f98
Jesus Esquer <jme2791@gmail.com>
2020-03-02 12:17:33 -0700

    Updated javascript added variable for canvas context and height and width, got random x and y positions

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,18 @@
 // Select elements on page
-// Canvas
+// Canvas and Context from canvas
 const canvas = document.querySelector('#etch-a-sketch');
-console.log(canvas)
+const ctx = canvas.getContext('2d');
 // Shake button
 const btn = document.querySelector('.shake');
+const moveAmount = 10;
 console.log(btn)
-
+// Grab width and height from canvas deconstructing
+const {width, height} = canvas;
 // Setup canvas for drawing
-
+// Start at random position for x and y
+let x = Math.floor(Math.random()* width);
+let y = Math.floor(Math.random()* height);
+console.log(x,y)
 // Write a draw function
 
 // Write a handler for keys
