e615743a69d96c97fc11a942efa02001584296d3
Jesus Esquer <jme2791@gmail.com>
2020-03-02 12:29:28 -0700

    Updated javascript added methods to control where and how lines are drawn in canvas

--- assets/index.js
+++ assets/index.js
@@ -5,14 +5,30 @@ const ctx = canvas.getContext('2d');
 // Shake button
 const btn = document.querySelector('.shake');
 const moveAmount = 10;
-console.log(btn)
 // Grab width and height from canvas deconstructing
 const {width, height} = canvas;
 // Setup canvas for drawing
 // Start at random position for x and y
 let x = Math.floor(Math.random()* width);
 let y = Math.floor(Math.random()* height);
-console.log(x,y)
+// We will use this variable for color hsl
+let hue = 0;
+//The following methods control how lines are drawn in canvas context.
+//Defines the type of corners where two lines meet.
+ctx.lineJoin = 'round';
+//Type of endings on the end of lines.
+ctx.lineCap = 'round';
+ctx.lineWidth = moveAmount;
+// Color of line
+ctx.strokeStyle = `hsl(${hue}, 100%, 50%)`
+//Starts a new path by emptying the list of sub-paths
+ctx.beginPath();
+//Moves the starting point of a new sub-path to the (x, y) coordinates.
+ctx.moveTo(x,y);
+//Connects the last point in the current sub-path to the specified (x, y) coordinates with a straight line.
+ctx.lineTo(x,y);
+//Strokes the current sub-paths with the current stroke style.
+ctx.stroke();
 // Write a draw function
 
 // Write a handler for keys
